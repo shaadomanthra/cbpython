001ec8d51bfc68ec52892414348feeb678db98ed
Krishna Teja <packetcode@gmail.com>
2020-05-28 12:16:44 +0530

    few changes

--- batch1/day12/helloapp/controller/HelloController.py
+++ batch1/day12/helloapp/controller/HelloController.py
@@ -6,17 +6,19 @@ class Hello:
         self.view = view
 
     def hello_basic(self):
-        self.view.l1.config(text="I have changed it")
+        self.view.l1.config(text="Hello Basic is Clicked")
         query = "INSERT INTO entries (message) VALUES('hello basic')"
         self.hellodb.insert(query)
         print("Hello")
 
     def hello_name(self):
+        self.view.l1.config(text="Hello Name is Clicked")
         query = "INSERT INTO entries (message) VALUES('hello name')"
         self.hellodb.insert(query)
         print("Hello Teja")
 
     def hello_in_french(self):
+        self.view.l1.config(text="Hello in french is Clicked")
         query = "INSERT INTO entries (message) VALUES('hello in french')"
         self.hellodb.insert(query)
         print("Bonjour")
--- batch1/day12/helloapp/view/LayoutView.py
+++ batch1/day12/helloapp/view/LayoutView.py
@@ -7,8 +7,14 @@ class Layout:
         self.root = Tk()
         h = Hello(self)
 
-        self.b1 = Button(self.root, text="click me", command = h.hello_basic)
+        self.b1 = Button(self.root, text=" hello basic ", command = h.hello_basic)
         self.b1.pack()
+
+        self.b2 = Button(self.root, text="hello name", command=h.hello_name)
+        self.b2.pack()
+
+        self.b3 = Button(self.root, text="hello in french", command=h.hello_in_french)
+        self.b3.pack()
 
         self.l1 = Label(self.root, text="Welcome")
         self.l1.pack()
