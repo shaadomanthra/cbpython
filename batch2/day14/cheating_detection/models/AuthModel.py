1241d7925543ed195a37dabc3854d5c4cbb806e5
Krishna Teja <packetcode@gmail.com>
2020-06-02 19:41:37 +0530

    few changes

--- batch2/day14/cheating_detection/models/AuthModel.py
+++ batch2/day14/cheating_detection/models/AuthModel.py
@@ -3,7 +3,13 @@ from db import *
 class AuthModel:
 
     def __init__(self):
-        print("Hello This is from AuthModel")
+        self.conn = connect("app.db")
 
+    def getUser(self,username,password):
+        query = f"SELECT * FROM users WHERE username='{username}' and password='{password}' "
+        result = fetchone(self.conn,query)
+        print(result)
 
 am = AuthModel()
+am.getUser('ramesh','ramesh123')
+
