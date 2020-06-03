0d8449348f80b73eb7f0209fd380b9adb324dccf
Krishna Teja <packetcode@gmail.com>
2020-06-03 19:10:24 +0530

    few changes

---
+++ batch2/day15/video.py
@@ -0,0 +1,20 @@
+# import the computer vision library
+import cv2
+
+# enable the webcamera
+cap = cv2.VideoCapture(0)
+
+# do a continuous image capture
+while True:
+    ret, frame = cap.read()  # This will take one image from webcam
+    # push the imaage on the window
+    cv2.imshow('Video', frame)
+
+    # do this as long as someone presses the escape key for 1 sec
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+#  release the webcamera & destroy the window
+cap.release()
+cv2.destroyAllWindows()
